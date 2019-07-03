7a3f0f9322aba166ea892acdb7e0f28643eea4b7
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2019-07-03 17:32:39 -0300

    Se ajusta el model CategoryItem, se crean datafixture

--- Modules/App/Models/CategoryItem.py
+++ Modules/App/Models/CategoryItem.py
@@ -8,11 +8,11 @@ class BaseManager(models.Manager):
 
 class CategoryItem(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    english = models.CharField(max_length=50, null=True)
-    spanish = models.CharField(max_length=50, null=True)
-    image = models.ImageField(upload_to="images", max_length=100, null=True)
-    audio = models.FileField(upload_to="audio", max_length=100, null=True)
-    color = models.CharField(max_length=30)
+    en = models.CharField(max_length=50, null=True)
+    es = models.CharField(max_length=50, null=True)
+    img = models.ImageField(upload_to="images", max_length=100, null=True)
+    snd = models.FileField(upload_to="audio", max_length=100, null=True)
+    col = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.english
+        return self.en
